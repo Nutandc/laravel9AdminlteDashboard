35b1faf02c0d90c51f5b04dddcb6e7b5ccf63681
Nutan Dahal <dahalnutan66@gmail.com>
2022-11-25 12:38:16 +0545

    Update DatabaseSeeder.php
    removed foreign set

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,6 +41,6 @@ class DatabaseSeeder extends Seeder
 
         $this->command->info('Finished!');
 
-    	DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+//     	DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
